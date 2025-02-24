7e5e0e6f3120cfc4b0958400f799a8add35a8106
palak garg <gargpalak0401@gmail.com>
2025-02-24 17:08:38 +0530

    Post Request

--- PostRequest/server.js
+++ PostRequest/server.js
@@ -2,9 +2,26 @@
 //data is not visible in the url
 const express = require("express");
 const app = express();
+//express.urlencoded : It is a Middleware that passes urlencoded data into object
+app.use(express.urlencoded({extended : true}))
+
+let userdata = [];
 
 app.get("/adduser", (req, res) => {
     res.sendFile(__dirname + "/Register.html");
+});
+
+app.post("/adduser", (req, res) => {
+    let {name, email, password} = req.body;
+    let newUser = {
+        name : name,
+        email : email,
+        password : password
+    };
+    userdata.push(newUser);
+    //res.send(`<center><h1>User Added</h1> <br/> ${userdata} </center>`);
+    res.send(userdata);
+    console.log(name, email, password);
 });
 app.listen(4500, ()=>{
     console.log("Server started on Port 4500...");
