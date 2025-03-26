3d77e623e61f62af4d9e55957b5b2e8de4b0c621
palak garg <gargpalak0401@gmail.com>
2025-03-26 16:57:25 +0530

    RestfulAPI

--- Mongoose/server.js
+++ Mongoose/server.js
@@ -6,6 +6,7 @@ const User = require("./model/user");
 
 mongoose.connect('mongodb://127.0.0.1:27017').then(() => console.log('Mongodb Connected!'));
 
+//Create User
 app.post("/users", async(req, res) => {
     let {name, email, password} = req.body; //reading data from form
     let newUser = new User({
@@ -16,6 +17,22 @@ app.post("/users", async(req, res) => {
     await newUser.save(); //asynchoronus (database quesries) //await is used to make asynchoronus to sychoronus
     res.send("User added");
 
+});
+
+//read all users
+app.get("/users", async(req, res) => {
+    let allUsers = await User.find();
+    res.send(allUsers);
+});
+
+//read one user
+app.get("/users/:id", async(req, res) => {
+    let {id} = req.params;
+    let userById = await User.findById(id);
+    if(!userById){
+        return res.status(400).send({error : "User not found"});
+    }
+    res.send(userById);
 });
 
 app.listen(port, () => {
